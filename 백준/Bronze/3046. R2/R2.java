8e9d1265b9e7c4b33c103c697f12d267623156f1
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2023-08-19 16:04:11 +0900

    [Bronze IV] Title: R2, Time: 220 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int r1 = sc.nextInt(), s = sc.nextInt();
+		int r2 =0;
+		r2=(s*2)-r1;
+		System.out.println(r2);
+	}
+}
