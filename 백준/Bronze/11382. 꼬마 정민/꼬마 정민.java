10cc1388d4b97ac053739dd87857bb2b3dda4d81
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2023-08-19 16:49:36 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);	
+		long i = sc.nextLong(),j = sc.nextLong(),k = sc.nextLong(); 
+		System.out.println(i+j+k); //결과값
+	}
+}
