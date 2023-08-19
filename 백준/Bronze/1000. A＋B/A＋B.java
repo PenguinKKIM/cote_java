94e66fbe26ebd9dc0818ed7a3b433a793e051193
PenguinKKIM <103123935+PenguinKKIM@users.noreply.github.com>
2023-08-19 16:03:02 +0900

    [Bronze V] Title: A+B, Time: 220 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int i = sc.nextInt();
+		int j = sc.nextInt();
+		System.out.println(+(i+j));
+	}
+}
